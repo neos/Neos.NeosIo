a12260bc5be2f33aa927745726d155ae136ef7cd
Rens Admiraal <rens.admiraal@typo3.org>
2012-06-23 18:49:45 +0200

    [TASK] Add Surf deployment script
    This change introduces an initial version of a surf deployment script
    for phoenix.typo3.org
    
    - Updates sourcecode
    - Imports site package
    - Renders documentation and imports it to the site
    
    Change-Id: I20db33761ae491187dd7ed04c0071b168bc21b76

---
+++ Build/Surf/phoenix-typo3-org.php
@@ -0,0 +1,50 @@
+<?php
+use \TYPO3\Surf\Domain\Model\Workflow;
+use \TYPO3\Surf\Domain\Model\Node;
+use \TYPO3\Surf\Domain\Model\SimpleWorkflow;
+
+/**
+ * For this deployment the following env variables are required:
+ *
+ * DEPLOYMENT_PATH: path on the remote server to deploy to
+ * DEPLOYMENT_USER: username to connect to the remote server
+ */
+
+$application = new \TYPO3\Surf\Application\TYPO3();
+if (getenv('DEPLOYMENT_PATH')) {
+	$application->setDeploymentPath(getenv('DEPLOYMENT_PATH'));
+} else {
+	throw new \Exception('Deployment path must be set in the DEPLOYMENT_PATH env variable.');
+}
+
+$application->setOption('repositoryUrl', 'git://git.typo3.org/Sites/PhoenixTypo3Org.git');
+$application->setOption('sitePackageKey', 'TYPO3.PhoenixTypo3Org');
+$application->setOption('keepReleases', 20);
+
+$deployment->addApplication($application);
+
+$workflow = new SimpleWorkflow();
+# $workflow->setEnableRollback(FALSE);
+$deployment->setWorkflow($workflow);
+
+$deployment->onInitialize(function() use ($workflow, $application) {
+	$workflow->removeTask('typo3.surf:flow3:setfilepermissions');
+	$workflow->removeTask('typo3.surf:flow3:copyconfiguration');
+});
+
+$workflow->defineTask('x:renderdocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:render'));
+$workflow->defineTask('x:importdocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:import'));
+$workflow->afterTask('typo3.surf:typo3:importsite', array('x:renderdocumentation', 'x:importdocumentation'));
+
+#$workflow->afterTask('typo3.surf:symlinkrelease', array('typo3.surf:varnishpurge'), $application);
+
+$node = new Node('phoenix.typo3.org');
+$node->setHostname('phoenix.typo3.org');
+if (getenv('DEPLOYMENT_USERNAME')) {
+	$node->setOption('username', getenv('DEPLOYMENT_USERNAME'));
+} else {
+	throw new \Exception('Username must be set in the DEPLOYMENT_USERNAME env variable.');
+}
+
+$application->addNode($node);
+?>
