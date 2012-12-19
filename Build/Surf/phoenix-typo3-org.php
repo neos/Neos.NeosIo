6db1b603728440414c42a01fcf88770bf59d74d0
Karsten Dambekalns <karsten@typo3.org>
2012-12-19 12:37:13 +0100

    [TASK] Update Surf descriptor for neos.typo3.org
    Change-Id: Ied33b4d31ec169a309d4f7a9a256d06de9be44ea

--- Build/Surf/phoenix-typo3-org.php
+++ Build/Surf/phoenix-typo3-org.php
@@ -8,6 +8,7 @@ use \TYPO3\Surf\Domain\Model\SimpleWorkflow;
  *
  * DEPLOYMENT_PATH: path on the remote server to deploy to
  * DEPLOYMENT_USER: username to connect to the remote server
+ * DEPLOYMENT_HOST: node host name
  */
 
 $application = new \TYPO3\Surf\Application\TYPO3();
@@ -20,6 +21,8 @@ if (getenv('DEPLOYMENT_PATH')) {
 $application->setOption('repositoryUrl', 'git://git.typo3.org/Sites/PhoenixTypo3Org.git');
 $application->setOption('sitePackageKey', 'TYPO3.NeosTypo3Org');
 $application->setOption('keepReleases', 20);
+$application->setOption('composerCommandPath', 'php /var/www/vhosts/neos.typo3.org/home/composer.phar');
+
 
 $deployment->addApplication($application);
 
@@ -30,17 +33,25 @@ $deployment->setWorkflow($workflow);
 $deployment->onInitialize(function() use ($workflow, $application) {
 	$workflow->removeTask('typo3.surf:flow3:setfilepermissions');
 	$workflow->removeTask('typo3.surf:flow3:copyconfiguration');
+	$workflow->removeTask('typo3.surf:typo3:importsite');
 });
 
 $workflow->defineTask('x:renderdocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:render --bundle NeosDocumentation'));
 $workflow->defineTask('x:importgettingstarteddocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:import --bundle NeosGettingStarted'));
 $workflow->defineTask('x:importfeaturedocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:import --bundle NeosFeatures'));
-$workflow->afterTask('typo3.surf:typo3:importsite', array('x:renderdocumentation', 'x:importgettingstarteddocumentation', 'x:importfeaturedocumentation'));
+$workflow->defineTask('x:importchangesdocumentation', 'typo3.surf:flow3:runcommand', array('command' => 'documentation:import --bundle NeosChanges'));
+$workflow->afterTask('typo3.surf:symlinkrelease', array('x:renderdocumentation', 'x:importgettingstarteddocumentation', 'x:importfeaturedocumentation', 'x:importchangesdocumentation'));
 
 #$workflow->afterTask('typo3.surf:symlinkrelease', array('typo3.surf:varnishpurge'), $application);
 
-$node = new Node('phoenix.typo3.org');
-$node->setHostname('phoenix.typo3.org');
+if (getenv('DEPLOYMENT_HOST')) {
+	$hostName = getenv('DEPLOYMENT_HOST');
+} else {
+	throw new \Exception('Deployment host name must be set in the DEPLOYMENT_HOST env variable.');
+}
+
+$node = new Node($hostName);
+$node->setHostname($hostName);
 if (getenv('DEPLOYMENT_USERNAME')) {
 	$node->setOption('username', getenv('DEPLOYMENT_USERNAME'));
 } else {
