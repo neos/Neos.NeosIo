3a894ded241dfc932db36bc75e3770fd195174c3
Karsten Dambekalns <karsten@neos.io>
2015-07-14 16:20:44 +0200

    [FEATURE] Add documentation content elements
    The "Documentation Library" can be used to show all projects of a given
    user on readthedocs.org.
    
    The "Project Documentation" element can be used to show a single
    projects' documentation.

---
+++ Packages/Sites/Neos.NeosIo/Classes/Neos/NeosIo/TypoScript/RtdLibraryImplementation.php
@@ -0,0 +1,80 @@
+<?php
+namespace Neos\NeosIo\TypoScript;
+
+/*
+ * The Neos project is licensed under GPL v3 or later
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Utility\Arrays;
+use TYPO3\TypoScript\TypoScriptObjects\Helpers\FluidView;
+
+/**
+ * RtdLibraryImplementation
+ */
+class RtdLibraryImplementation extends \TYPO3\TypoScript\TypoScriptObjects\TemplateImplementation {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Http\Client\Browser
+	 */
+	protected $browser;
+
+	/**
+	 * @return void
+	 */
+	protected function initializeObject() {
+		$this->browser->setRequestEngine(new \TYPO3\Flow\Http\Client\CurlEngine());
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function initializeView(FluidView $fluidView) {
+		$user = $this->getUserData($this['username']);
+		$excludedProjects = Arrays::trimExplode(',', $this['excludedprojects']);
+		$decodedProjects = $this->getProjectsData($user['id']);
+
+		$projects = [];
+		foreach ($decodedProjects['objects'] as $project) {
+			if (!in_array($project['slug'], $excludedProjects)) {
+				$projects[] = $project;
+			}
+		}
+
+		$fluidView->assign('projects', $projects);
+	}
+
+	/**
+	 * @param string $username
+	 * @return array
+	 * @throws \TYPO3\Flow\Http\Client\InfiniteRedirectionException
+	 */
+	protected function getUserData($username) {
+		$response = $this->browser->request(sprintf('http://readthedocs.org/api/v1/user/%s/?format=json', $username));
+		$userData = json_decode($response, TRUE);
+
+		return $userData;
+	}
+
+	/**
+	 * @param integer $userId
+	 * @param integer $limit
+	 * @param integer $offset
+	 * @return array
+	 * @throws \TYPO3\Flow\Http\Client\InfiniteRedirectionException
+	 */
+	protected function getProjectsData($userId = NULL, $limit = NULL, $offset = NULL) {
+		$query = http_build_query([
+			'users' => $userId,
+			'limit' => $limit,
+			'offset' => $offset,
+			'format' => 'json'
+		]);
+		$response = $this->browser->request(sprintf('http://readthedocs.org/api/v1/project/?%s', $query));
+		$projectsData = json_decode($response, TRUE);
+
+		return $projectsData;
+	}
+
+}
