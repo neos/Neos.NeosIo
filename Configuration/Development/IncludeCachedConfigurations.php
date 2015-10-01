47ff390b36b9778f6f4a2de2a75807d2771cbefa
Aske Ertmann <aertmann@gmail.com>
2015-10-01 13:11:23 +0200

    [BUGFIX] Fix and optimize .gitignore & remove cached autogenerated configuration

--- Configuration/Development/IncludeCachedConfigurations.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (FLOW_PATH_ROOT !== '/Users/jensen/Sites/Neos.NeosIo/' || !file_exists('/Users/jensen/Sites/Neos.NeosIo/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php')) {
-	unlink(__FILE__);
-	return array();
-}
-return require '/Users/jensen/Sites/Neos.NeosIo/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php';
