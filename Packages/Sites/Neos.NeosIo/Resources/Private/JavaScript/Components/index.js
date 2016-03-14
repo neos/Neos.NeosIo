c430caf2ffc65fe45558c433781a690bad3b8d67
Tyll Weiß <inkdpixels@googlemail.com>
2016-03-14 21:00:20 +0100

    TASK: Initial work for responsive behavior for all static templates

--- Packages/Sites/Neos.NeosIo/Resources/Private/JavaScript/Components/index.js
+++ Packages/Sites/Neos.NeosIo/Resources/Private/JavaScript/Components/index.js
@@ -1,8 +1,10 @@
+import ClassToggler from './ClassToggler.js';
 import CountUp from './CountUp.js';
 import SentenceSwitcher from './SentenceSwitcher.js';
 import ScrollClassToggler from './ScrollClassToggler.js';
 
 export {
+	ClassToggler,
 	CountUp,
 	SentenceSwitcher,
 	ScrollClassToggler
