f0ad5aa22be8bfe480fb66c2b1028bd1770f3f4a
Sebastian Helzle <sebastian@helzle.it>
2016-11-24 17:16:01 +0100

    TASK: Lazy load images
    This change adds the layzr lazy load library which
    loads images when necessary.
    
    Img tags can have the following data-attributes to allow lazyloading:
    
    * data-image-normal (required)
    * data-image-retina
    * data-image-srcset
    
    The normal src attribute should be set to use a simple loading gif.
    
    Resolves: #122

--- Packages/Sites/Neos.NeosIo/Resources/Private/JavaScript/index.js
+++ Packages/Sites/Neos.NeosIo/Resources/Private/JavaScript/index.js
@@ -2,11 +2,25 @@ import 'babel-polyfill';
 import assembler from '@reduct/assembler';
 import * as siteComponents from './Components/';
 import * as marketPlaceComponents from '../../../../../Application/Neos.MarketPlace/Resources/Private/JavaScript/Components/';
+import Layzr from 'layzr.js';
 
 const app = assembler();
+const layzr = Layzr({
+    normal: 'data-image-normal',
+    retina: 'data-image-retina',
+    srcset: 'data-image-srcset',
+    threshold: 10
+});
 
 app.registerAll(siteComponents);
 app.registerAll(marketPlaceComponents);
 
 setTimeout(() => app.run(), 0);
 document.addEventListener('Neos.PageLoaded', app.run());
+
+setTimeout(() => {
+    layzr
+    .update()
+    .check()
+    .handlers(true);
+}, 0);
