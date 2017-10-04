8eb8f54ee22ba70c399e225d02f7563cf1f132bf
Robert Lemke <robert@flownative.com>
2017-10-04 08:53:44 +0200

    TASK: Pin Docker images and set PHP memory limit

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -9,7 +9,7 @@ fi
 docker-compose up -d --remove-orphans
 docker cp . "${BEACH_DEV_PROJECT_NAME}_php_1:/application/"
 docker exec "${BEACH_DEV_PROJECT_NAME}_php_1" /bin/bash -c "chown -R beach:beach /application"
+docker exec "${BEACH_DEV_PROJECT_NAME}_php_1" /bin/bash -c "rm -rf /application/Data/Temporary"
 docker exec "${BEACH_DEV_PROJECT_NAME}_sidecar_1" /bin/bash -c "cd /application; ./flow doctrine:migrate"
-
 
 echo "You can now open http://localhost:4080 in a browser. Have a neos day!"
