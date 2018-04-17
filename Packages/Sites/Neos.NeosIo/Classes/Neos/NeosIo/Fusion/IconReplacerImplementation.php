44c115ca4f96c59d3815bfae59016a8aaccad36e
Bastian Heist <bastian@Bastians-MacBook-Pro-2.local>
2018-04-17 07:49:57 +0200

    FEATURE: Replace [icon-something] with the corresponding fontawesome icon in text nodetype
    You can now write e.g. [icon-globe] in text in the backend, and it will be replaced by a fontawesome icon in the text. This is great for the user guide, in order to point the user to the icons he needs to click on.

---
+++ Packages/Sites/Neos.NeosIo/Classes/Neos/NeosIo/Fusion/IconReplacerImplementation.php
@@ -0,0 +1,58 @@
+<?php
+namespace Neos\NeosIo\Fusion;
+
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+/**
+ * A Fusion Menu object
+ */
+class IconReplacerImplementation extends AbstractFusionObject
+{
+    /**
+     * The string where replacing is done.
+     *
+     * @return mixed
+     */
+    public function getHaystack()
+    {
+        return $this->fusionValue('haystack');
+    }
+
+    /**
+     * The tag name with which the searched string is replaced
+     *
+     * @return string
+     */
+    public function getTagName()
+    {
+        return $this->fusionValue('tagName');
+    }
+
+    /**
+     * The class stub which is set on the replaced tags, appended with the preg match
+     *
+     * @return string
+     */
+    public function getClassStub()
+    {
+        return $this->fusionValue('classStub');
+    }
+
+    /**
+     * Replaces all occurences of [icon-xyz] with a tag generated from the input values.
+     *
+     * @return string The original text with icon placeholders replaced by tags.
+     */
+    public function evaluate()
+    {
+        $tagName = $this->getTagName();
+        $classStub = $this->getClassStub();
+        return preg_replace_callback(
+            "/\[icon-(?<replace>[\w-]*)\]/i",
+            function($match) use ($tagName, $classStub) {
+                return '<'.$tagName.' class="'.$classStub.strtolower($match['replace']).'"></'.$tagName.'>';
+            },
+            $this->getHaystack()
+        );
+    }
+}
