fe9f229936c415761c147058691cb12d48b2d5a9
Sebastian Helzle <sebastian@helzle.it>
2019-11-08 09:38:03 +0100

    TASK: Add some comments to OSM code

--- DistributionPackages/Neos.NeosIo.ServiceOfferings/Resources/Private/JavaScript/MarkerClusters.ts
+++ DistributionPackages/Neos.NeosIo.ServiceOfferings/Resources/Private/JavaScript/MarkerClusters.ts
@@ -7,12 +7,14 @@ declare global {
     }
 }
 
+// Position adjustments for Neos marker icon in map
 L.Icon.Default.prototype.options.iconSize = [24, 27];
 L.Icon.Default.prototype.options.iconAnchor = [12, 13];
 L.Icon.Default.prototype.options.shadowAnchor = [12, 27];
 L.Icon.Default.prototype.options.tooltipAnchor = [16, 0];
 L.Icon.Default.prototype.options.popupAnchor = [0, -20];
 
+// Define global hook for OSM plugin to add clusters
 export default () => {
     window.clusterMarkersHook = (layer) => {
         const clusterMarkers = L.markerClusterGroup();
@@ -20,4 +22,5 @@ export default () => {
     };
 }
 
+// Load OSM plugin code
 import 'Packages/Plugins/WebExcess.OpenStreetMap/Resources/Private/Assets/Main';
