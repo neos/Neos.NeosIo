d59634adf01e3073cd32220691f87404f1661931
Sebastian Helzle <sebastian@helzle.it>
2023-07-28 23:22:01 +0200

    BUGFIX: Make migrations output problems and continue

--- DistributionPackages/Neos.NeosIo/Classes/ContentRepository/Transformations/FlattenCollectionsTransformation.php
+++ DistributionPackages/Neos.NeosIo/Classes/ContentRepository/Transformations/FlattenCollectionsTransformation.php
@@ -3,34 +3,41 @@ declare(strict_types=1);
 
 namespace Neos\NeosIo\ContentRepository\Transformations;
 
-use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Factory\NodeFactory;
 use Neos\ContentRepository\Domain\Model\NodeData;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\ContentRepository\Migration\Transformations\AbstractTransformation;
-use Neos\Neos\Controller\CreateContentContextTrait;
+use Neos\Flow\Annotations as Flow;
 
 /**
  * Move content of elements with a single content collection to the parent to cleanup the content tree
  */
 class FlattenCollectionsTransformation extends AbstractTransformation
 {
-    use CreateContentContextTrait;
+    /**
+     * @Flow\Inject
+     * @var NodeFactory
+     */
+    protected $nodeFactory;
 
     public function isTransformable(NodeData $node): bool
     {
-        $numberOfChildNodes = $node->getNumberOfChildNodes('Neos.Neos:ContentCollection', $node->getWorkspace(), $node->getDimensions());
+        $numberOfChildNodes = $node->getNumberOfChildNodes(
+            'Neos.Neos:ContentCollection',
+            $node->getWorkspace(),
+            $node->getDimensionValues()
+        );
         return ($numberOfChildNodes === 1);
     }
 
     public function execute(NodeData $node): void
     {
-        $contentContext = $this->createContentContext($node->getWorkspace()->getName(), $node->getDimensionValues());
-        $parentNode = $contentContext->getNodeByIdentifier($node->getIdentifier());
+        $parentNode = $this->nodeFactory->createFromNodeData($node, $this->nodeFactory->createContextMatchingNodeData($node));
         if (!$parentNode) {
             return;
         }
 
         $contentCollections = $parentNode->getChildNodes('Neos.Neos:ContentCollection');
-
         foreach ($contentCollections as $contentCollection) {
             if ($contentCollection->hasChildNodes() && $contentCollection->getNodeType()->getName() === 'Neos.Neos:ContentCollection') {
                 $this->moveChildNodesToParent($contentCollection->getChildNodes(), $parentNode);
@@ -45,7 +52,17 @@ class FlattenCollectionsTransformation extends AbstractTransformation
     {
         foreach ($children as $childNode) {
             if ($childNode instanceof NodeInterface) {
-                $childNode->moveInto($parentNode);
+                try {
+                    $childNode->moveInto($parentNode);
+                } catch (\Exception $exception) {
+                    \Neos\Flow\var_dump([
+                        'contextPath' => $parentNode->getContextPath(),
+                        'workspace' => $parentNode->getWorkspace()->getName(),
+                        'dimensions' => json_encode($parentNode->getDimensions()),
+                        'nodetype' => $parentNode->getNodeType()->getName(),
+                        'exception' => $exception->getMessage(),
+                    ], 'Could not move node ' . $parentNode->getIdentifier());
+                }
             }
         }
     }
