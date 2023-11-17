2d7e74da415c9de459c5b2ee8e3c60345b89f75b
Manuel Meister <manuel.meister@jvm.ch>
2023-11-17 11:48:51 +0100

    Task: Create button basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,50 @@
 const content = require("./Build/Carbon.Pipeline/purge");
+const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content,
     theme: {
+        gradient: {
+            blue: "#26224C 0%, #0114C2 100%"
+        },
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: "#000",
+            white: "#fff",
+            darkblue: "#26224C",
+            lightblue: "#009FE3",
+            gray: {
+                DEFAULT: "#989898",
+                light: "#F1F1F1",
+            },
+            green: {
+                DEFAULT: "#2AA72A",
+                dark: "#1D751D",
+            },
+            accent: {
+                purple: "#8A3FFC",
+                blue: "#0114C2",
+                lightblue: "#00CFFD",
+                green: "#35BA34",
+                yellow: "#F1C21B",
+                orange: "#FF832B",
+                red: "#FA4D56",
+            }
+        },
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        plugin(function ({addUtilities, e, theme}) {
+            const gradients = theme('gradients', {})
+            const utilities = Object.entries(gradients).map(([name, gradient]) => ({
+                [`.${e(`gradient-${name}`)}`]: {
+                    '--gradient-color-stops': gradient
+                }
+            }));
+
+            addUtilities(utilities);
+        }),
+    ],
 };
