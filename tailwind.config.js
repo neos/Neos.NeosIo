8634096a97643511e7c5524797a21f4b746cdf26
rtp-cgs <59559730+rtp-cgs@users.noreply.github.com>
2023-11-21 15:11:25 +0100

    FEATURE: Adding logo bar atom (#544)
    * [TASK] Adding fusion template for LogoBar atom
    
    * Using map for compact styleguide data & improve propTypes

--- tailwind.config.js
+++ tailwind.config.js
@@ -92,7 +92,7 @@ module.exports = {
         },
     },
     plugins: [
-        plugin(function ({ addUtilities, e, theme }) {
+        plugin(({ addUtilities, e, theme }) => {
             const gradients = theme('gradients', {});
             const utilities = Object.entries(gradients).map(([name, gradient]) => ({
                 [`.${e(`gradient-${name}`)}`]: {
@@ -101,6 +101,23 @@ module.exports = {
             }));
 
             addUtilities(utilities);
+        }),
+        // Add scrollbar variants and utilities
+        plugin(({ addVariant, addUtilities }) => {
+            addVariant('scrollbar', '&::-webkit-scrollbar');
+            addVariant('scrollbar-track', '&::-webkit-scrollbar-track');
+            addVariant('scrollbar-thumb', '&::-webkit-scrollbar-thumb');
+
+            addUtilities({
+                // Add utility to hide the scrollbar
+                '.hide-scrollbar': {
+                    '-ms-overflow-style': 'none', // for Internet Explorer, Edge
+                    'scrollbar-width': 'none', // for Firefox
+                    '&::-webkit-scrollbar': {
+                        display: 'none', // for Chrome, Safari, and Opera
+                    },
+                },
+            });
         }),
     ],
 };
