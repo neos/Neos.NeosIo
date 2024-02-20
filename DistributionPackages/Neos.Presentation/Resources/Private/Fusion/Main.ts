843dd2d1429eb3e3d0110b72a08e09d680e4e72b
jwiegand <j.wiegand@muensmedia.de>
2024-02-20 12:35:03 +0100

    [TASK] optimize LogoBar

--- DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Main.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Main.ts
@@ -5,7 +5,7 @@ import intersect from "@alpinejs/intersect";
 import collapse from "@alpinejs/collapse";
 import clipboard from "@ryangjchandler/alpine-clipboard";
 import typewriter from "@marcreichel/alpine-typewriter";
-import LogoBar from './Molecule/LogoBar/LogoBar';
+import './Molecule/LogoBar/LogoBar';
 
 // @ts-ignore
 Alpine.plugin([
@@ -18,8 +18,6 @@ Alpine.plugin([
 ]);
 
 window.Alpine = Alpine;
-
-Alpine.data('LogoBar', LogoBar);
 
 Alpine.start();
 
--- DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Molecule/LogoBar/LogoBar.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Molecule/LogoBar/LogoBar.ts
@@ -1,13 +1,14 @@
-export default () => ({
+import Alpine from 'alpinejs';
+
+Alpine.data('LogoBar', () => ({
     init() {
-        let $list = this.$el.querySelector('.item-list');
-        $list.replaceChildren(...[...$list.children].sort(() => Math.random() - 0.5));
+        const logos = this.$refs.logos;
+        logos.replaceChildren(...[...logos.children].sort(() => Math.random() - 0.5));
         this.$nextTick(() => {
-            let ul = this.$refs.logos;
             for (let i = 1; i <= 3; i++) {
-                ul.insertAdjacentHTML('afterend', ul.outerHTML);
-                ul.nextSibling.setAttribute('aria-hidden', 'true');
+                logos.insertAdjacentHTML('afterend', logos.outerHTML);
+                logos.nextSibling.setAttribute('aria-hidden', 'true');
             }
         });
     },
-});
+}));
