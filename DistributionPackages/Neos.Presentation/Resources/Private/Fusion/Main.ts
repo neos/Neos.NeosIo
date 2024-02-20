65105c94eeae460c8d4aea2cdbb3af2b178fc9ab
Jon Uhlmann <account@uhlmann.pro>
2024-02-20 14:59:57 +0100

    TASK: Remove import
    It will be re-added later

--- DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Main.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Main.ts
@@ -1,23 +1,14 @@
-import Alpine from "alpinejs";
-import anchor from "@alpinejs/anchor";
-import focus from "@alpinejs/focus";
-import intersect from "@alpinejs/intersect";
-import collapse from "@alpinejs/collapse";
-import clipboard from "@ryangjchandler/alpine-clipboard";
-import typewriter from "@marcreichel/alpine-typewriter";
-import './Molecule/LogoBar/LogoBar';
+import Alpine from 'alpinejs';
+import anchor from '@alpinejs/anchor';
+import focus from '@alpinejs/focus';
+import intersect from '@alpinejs/intersect';
+import collapse from '@alpinejs/collapse';
+import clipboard from '@ryangjchandler/alpine-clipboard';
+import typewriter from '@marcreichel/alpine-typewriter';
 
 // @ts-ignore
-Alpine.plugin([
-    anchor,
-    clipboard,
-    collapse,
-    focus,
-    intersect,
-    typewriter,
-]);
+Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter]);
 
 window.Alpine = Alpine;
 
 Alpine.start();
-
