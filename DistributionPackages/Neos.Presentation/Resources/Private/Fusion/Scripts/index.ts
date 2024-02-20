662466288f628339dff923cb7de5c4869e6e66dc
Jon Uhlmann <account@uhlmann.pro>
2024-02-20 09:18:00 +0100

    TASK: Format document

--- DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Scripts/index.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Scripts/index.ts
@@ -1,20 +1,13 @@
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
 import anchor from './anchor';
-import focus from "@alpinejs/focus";
-import intersect from "@alpinejs/intersect";
-import collapse from "@alpinejs/collapse";
-import clipboard from "@ryangjchandler/alpine-clipboard";
-import typewriter from "@marcreichel/alpine-typewriter";
+import focus from '@alpinejs/focus';
+import intersect from '@alpinejs/intersect';
+import collapse from '@alpinejs/collapse';
+import clipboard from '@ryangjchandler/alpine-clipboard';
+import typewriter from '@marcreichel/alpine-typewriter';
 
 // @ts-ignore
-Alpine.plugin([
-    anchor,
-    clipboard,
-    collapse,
-    focus,
-    intersect,
-    typewriter,
-]);
+Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter]);
 
 window.Alpine = Alpine;
 
