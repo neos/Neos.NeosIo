c781897b30adb8364217dd0271867aa2e191deee
Manuel Meister <manuel.meister@jvm.ch>
2024-02-22 16:09:41 +0100

    TASK: Implement nav flyout

--- DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Scripts/index.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Fusion/Scripts/index.ts
@@ -5,6 +5,7 @@ import intersect from '@alpinejs/intersect';
 import collapse from '@alpinejs/collapse';
 import clipboard from '@ryangjchandler/alpine-clipboard';
 import typewriter from '@marcreichel/alpine-typewriter';
+import '../Organism/Navigation/Navigation.js';
 
 // @ts-ignore
 Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter]);
