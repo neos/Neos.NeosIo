35a571399d3893a882ab0d838a559b8b14f19ff6
Jon Uhlmann <account@uhlmann.pro>
2025-04-02 12:25:06 +0200

    TASK: Adjust build config

--- .postcssrc.mjs
+++ .postcssrc.mjs
@@ -1,25 +1,27 @@
-module.exports = (ctx) => ({
-    plugins: {
-        "postcss-import": {
-            resolve: ctx.resolve,
-        },
-        "tailwindcss/nesting": true,
-        tailwindcss: ctx.tailwindcss,
-        "postcss-assets": {
-            cachebuster: false,
-            basePath: `${ctx.basePath}/`,
-            baseUrl: "/_Resources/Static/Packages",
-            loadPaths: ["**/Resources/Public/**/*"],
-        },
-        "postcss-clip-path-polyfill": true,
-        autoprefixer: true,
-        cssnano: ctx.minify
-            ? {
+export default function (ctx) {
+    return {
+        plugins: {
+            "postcss-import": {
+                resolve: ctx.resolve,
+            },
+            "tailwindcss/nesting": true,
+            tailwindcss: ctx.tailwindcss,
+            "postcss-assets": {
+                cachebuster: false,
+                basePath: `${ctx.basePath}/`,
+                baseUrl: "/_Resources/Static/Packages",
+                loadPaths: ["**/Resources/Public/**/*"],
+            },
+            "postcss-clip-path-polyfill": true,
+            autoprefixer: true,
+            cssnano: ctx.minify
+                ? {
                       preset: ["default", { discardComments: { removeAll: true }, svgo: false, calc: false }],
-              }
-            : false,
-        "postcss-reporter": {
-            clearReportedMessages: true,
+                  }
+                : false,
+            "postcss-reporter": {
+                clearReportedMessages: true,
+            },
         },
-    },
-});
+    };
+}
