3aefddbc43da592cb1903953bc1f4dc673ee4207
Jon Uhlmann <account@uhlmann.pro>
2025-04-08 09:16:56 +0200

    TASK: Move assets to own folder

--- DistributionPackages/Neos.Presentation/Resources/Private/Assets/Scripts/index.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Assets/Scripts/index.ts
@@ -7,9 +7,9 @@ import intersect from '@alpinejs/intersect';
 import collapse from '@alpinejs/collapse';
 import clipboard from '@ryangjchandler/alpine-clipboard';
 import typewriter from '@marcreichel/alpine-typewriter';
-import '../Molecule/LogoBar/LogoBar';
-import '../Organism/ImageCollage';
-import '../Organism/Navigation/Navigation.js';
+import '../../Fusion/Molecule/LogoBar/LogoBar';
+import '../../Fusion/Organism/ImageCollage';
+import '../../Fusion/Organism/Navigation/Navigation.js';
 
 // @ts-ignore
 Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter]);
