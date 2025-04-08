d0c844607d442d1195b96622244840220218275a
Jon Uhlmann <account@uhlmann.pro>
2025-04-08 14:53:53 +0200

    TASK: Refactor Step by step highlight

--- DistributionPackages/Neos.Presentation/Resources/Private/Assets/Scripts/index.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Assets/Scripts/index.ts
@@ -7,12 +7,31 @@ import intersect from '@alpinejs/intersect';
 import collapse from '@alpinejs/collapse';
 import clipboard from '@ryangjchandler/alpine-clipboard';
 import typewriter from '@marcreichel/alpine-typewriter';
+import disclosure from './ui/disclosure';
 import '../../Fusion/Presentation/LogoBar';
 import '../../Fusion/Organism/ImageCollage';
 import '../../Fusion/Organism/Navigation/Navigation.js';
 
+Alpine.data('exclusiveDisclosure', function (id) {
+    return {
+        id,
+        get expanded() {
+            return this.active === this.id;
+        },
+        set expanded(id) {
+            this.active = id ? this.id : null;
+        },
+        init() {
+            Alpine.bind(this.$root, this.root);
+        },
+        root: {
+            ['x-model']: 'expanded',
+        },
+    };
+});
+
 // @ts-ignore
-Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter]);
+Alpine.plugin([anchor, clipboard, collapse, focus, intersect, typewriter, disclosure]);
 
 window.Alpine = Alpine;
 
