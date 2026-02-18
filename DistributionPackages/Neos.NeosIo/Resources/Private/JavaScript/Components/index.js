5e76ceb21493bea289e4d0a156d60b79c9945ee1
Michael <michael.berhorst@sandstorm-media.de>
2026-02-18 13:55:55 +0100

    TASK: Removed node type "headline switcher"
    Was not used anymore. Ok was used once but the js code is broken, and it does not render anything.
    
    Addresses: #628

--- DistributionPackages/Neos.NeosIo/Resources/Private/JavaScript/Components/index.js
+++ DistributionPackages/Neos.NeosIo/Resources/Private/JavaScript/Components/index.js
@@ -5,14 +5,12 @@ import FundingBadges from './FundingBadges';
 import ScrollTo from './ScrollTo';
 import ProgressiveImage from './ProgressiveImage';
 import ScrollClassToggler from './ScrollClassToggler';
-import SentenceSwitcher from './SentenceSwitcher';
 
 export {
     ClassToggler,
     CountUp,
     ScrollTo,
     FundingBadges,
-    SentenceSwitcher,
     ProgressiveImage,
     ScrollClassToggler,
 };
