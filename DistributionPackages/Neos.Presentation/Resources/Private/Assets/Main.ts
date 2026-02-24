74fb2908ceb6193501653fc6468657c0482fec5d
Robert Baruck <robert.baruck@sandstorm.de>
2026-02-24 08:48:57 +0100

    FEAUTURE: [M01] Integrate HeroStage.LandingPage into NeosIo

--- DistributionPackages/Neos.Presentation/Resources/Private/Assets/Main.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Assets/Main.ts
@@ -3,8 +3,10 @@ import '../Fusion/Presentation/LogoBar';
 import '../Fusion/Presentation/ImageCollage';
 import '../Fusion/Presentation/Navigation';
 import '../Fusion/Presentation/Slider';
+import { initStage } from '../Fusion/Presentation/Stage/Stage';
 
 // @ts-ignore: Set Alpine as a global variable
 window.Alpine = Alpine;
 
+initStage();
 Alpine.start();
