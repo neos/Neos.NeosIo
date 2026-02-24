e3d66eda68a0f15d2cce1e1438b6c5fa5d6a8a28
Michael <michael.berhorst@sandstorm-media.de>
2026-02-24 09:55:54 +0100

    TASK: Header/Navigation

--- DistributionPackages/Neos.Presentation/Resources/Private/Assets/Main.ts
+++ DistributionPackages/Neos.Presentation/Resources/Private/Assets/Main.ts
@@ -1,10 +1,15 @@
 import Alpine from './Scripts';
+import { AlpineComponent } from 'alpinejs'
 import '../Fusion/Presentation/LogoBar';
 import '../Fusion/Presentation/ImageCollage';
 import '../Fusion/Presentation/Navigation';
 import '../Fusion/Presentation/Slider';
 
+import MobileMenu, { MobileMenuComponent } from '../Fusion/Presentation/Navigation/Mobile/MobileNavigation';
+
 // @ts-ignore: Set Alpine as a global variable
 window.Alpine = Alpine;
+
+Alpine.data('mobileMenu', MobileMenu as (value: any) => AlpineComponent<MobileMenuComponent>)
 
 Alpine.start();
