29c5756f6c6f47c96082255f76abf5fc2a11c2c2
Michael <michael.berhorst@sandstorm.de>
2026-04-15 13:30:32 +0200

    TASK: Remove unused react and styles for case study filtering

--- DistributionPackages/Neos.NeosIo.CaseStudies/Resources/Private/JavaScript/index.ts
+++
@@ -1,9 +0,0 @@
-import CaseStudyListing from './CaseStudyListing';
-import CasesData from "./Context/CasesData";
-import TranslationData from "./Context/TranslationData";
-
-export {
-    CaseStudyListing,
-    CasesData,
-    TranslationData
-};
